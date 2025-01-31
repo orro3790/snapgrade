ae48d0396f2f849001629219c0935a80feb9b23d
Matt <mattorrock@gmail.com>
2025-01-31 18:33:58 +0900

    fix(svelte.config.js): enable SPA mode

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,12 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html', // Enable SPA mode
+			strict: false // Allow dynamic routes
+		}),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/snapgrade' : ''
 		}
