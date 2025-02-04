63a090fbe00a3be1d73ba3816e4ad71eb5a91d86
orro3790 <mattorrock@gmail.com>
2025-02-04 11:10:10 +0000

    chore(vercel.json): delete unecessary config

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,12 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			runtime: 'nodejs20.x'
-		})
-	},
-	preprocess: vitePreprocess()
+		adapter: adapter()
+	}
+	// preprocess: vitePreprocess()
 };
 
 export default config;
