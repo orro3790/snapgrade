a5f404ecf4d235e9b62eda7a5bd0879cb6e0a86b
orro3790 <mattorrock@gmail.com>
2025-02-04 09:00:44 +0000

    fix(config): fix config files for deployment to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
-	preprocess: vitePreprocess()
+  kit: {
+    adapter: adapter({
+      runtime: 'nodejs18'
+    })
+  },
+  preprocess: vitePreprocess()
 };
 
 export default config;
