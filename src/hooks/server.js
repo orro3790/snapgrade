1f76bb903fe3795c11cbb007345df9ca834cffc3
Matt <mattorrock@gmail.com>
2025-02-25 17:41:52 +0900

    chore(cleanup): Remove unused files

--- src/hooks/server.js
+++
@@ -1,6 +0,0 @@
-export const handle = async ({ event, resolve }) => {
-  if (!isRagRunning()) { // Check if service is alive
-    startRagService() // Launch via child_process
-  }
-  return resolve(event)
-} 
